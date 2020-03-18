7c5b4f4af7d2ae7ee64cf43af55299cc009ccaad
adriancreteanu <creteanu.adrian@gmail.com>
2020-03-18 23:59:44 +0200

    Remove some unused files

---
+++ Disertatie/Source/Network/APIConfiguration.swift
@@ -0,0 +1,17 @@
+
+//
+//  APIConfiguration.swift
+//  Disertatie
+//
+//  Created by Adrian Crețeanu on 18/03/2020.
+//  Copyright © 2020 Adrian Creteanu. All rights reserved.
+//
+
+import Alamofire
+
+protocol APIConfiguration: URLRequestConvertible {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+}
+
